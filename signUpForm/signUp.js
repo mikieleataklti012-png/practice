98fa948b380f152b1bff8b9bef8841dd1851d9be
mikieleataklti012-pgn <mikieleataklti012@gmail.com>
2026-08-30 18:55:49 +0300

    signup form

--- signUpForm/signUp.js
+++ signUpForm/signUp.js
@@ -1,16 +1,21 @@
 function confirmPassword(event){
-    event.preventDefault();
-
     let password=document.getElementById('password');
     let comfirmpass=document.getElementById('confirm');
     let error=document.querySelector('.error');
-    if(comfirmpass.value===password.value){
+    if(password.value!==""&& comfirmpass.value===password.value){
+        event.preventDefault();
         error.textContent="sign up successful";
+        return;
+    }
+    else if(password.value===""){
+        event.preventDefault();
+        error.textContent="password can not be empty";
+        return;
     }
     else{
         error.textContent="Password and Confirm Password dont match please do it again";
     }
 }
 
-let btn=document.getElementById('btn');
-btn.addEventListener('click',confirmPassword);
+let form=document.querySelector('form');
+form.addEventListener('submit',confirmPassword);
